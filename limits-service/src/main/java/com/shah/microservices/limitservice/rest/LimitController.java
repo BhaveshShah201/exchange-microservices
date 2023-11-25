d07e2274c1eff9f924aac7777c1f6c1941880631
Bhavesh Shah <bshah201@gmail.com>
2023-11-25 22:25:18 +0530

    Initial commits with cloud config

--- limits-service/src/main/java/com/shah/microservices/limitservice/rest/LimitController.java
+++ limits-service/src/main/java/com/shah/microservices/limitservice/rest/LimitController.java
@@ -1,17 +1,22 @@
 package com.shah.microservices.limitservice.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shah.microservices.limitservice.config.Configuration;
 import com.shah.microservices.limitservice.modal.Limits;
 
 @RestController
 @RequestMapping("/api")
 public class LimitController {
 
+	@Autowired
+	private Configuration configuration;
+
 	@GetMapping("/limits")
 	public Limits retreiveLimits() {
-		return new Limits(1, 100);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
